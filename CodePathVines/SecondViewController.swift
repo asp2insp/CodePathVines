a3e2d31f0aaeb974da7770b5a75111471146bc79
Josiah Gaskin <josiahgaskin@gmail.com>
2015-05-04 19:25:25 -0700

    Initial Project Commit

---
+++ CodePathVines/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CodePathVines
+//
+//  Created by Josiah Gaskin on 5/4/15.
+//  Copyright (c) 2015 Josiah Gaskin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
